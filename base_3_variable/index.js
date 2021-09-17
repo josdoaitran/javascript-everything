228d8b0410c8d208cf4bc89890e73f3aecffd336
Doai Tran <doai.tran@tyme.com>
2021-09-18 00:08:39 +0700

    example variable and constant javascript

---
+++ base_3_variable/index.js
@@ -0,0 +1,24 @@
+let name = "Doai Tran"
+
+console.log("fist javascript file")
+console.log(name)
+
+
+// Variable in Javascript
+
+// + Can not be a reserved word
+// + Should be meaningful
+// + can not start with a number (1name)
+// + can not contain a space or hyphen (full name or full_name)
+// + are case-sensitive
+
+let firstName, lastName ;
+let firstCar = "Toyota";
+let secondCar = "Huynday";
+
+// Constant
+
+const pi_number = 3.1415
+//  Getting error if we assign new value
+// pi_number = 3.14
+console.log(pi_number)
